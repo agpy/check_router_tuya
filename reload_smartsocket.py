3344ec749f7f1f2182b19ebfe20b3ccb097e22ff
agpy <31065777+agpy@users.noreply.github.com>
2022-10-06 00:00:50 +0300

    Create reload_smartsocket.py

---
+++ reload_smartsocket.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+import sys
+from tuyapy import TuyaApi
+import os
+import time
+import json
+import subprocess
+
+api = TuyaApi()
+
+def reload_smartplug(plug_config):
+
+    with open(plug_config) as config:
+        data = json.load(config)
+
+    username,password,country_code,application,sw_device = data['username'],data['password'],data['country_code'],data['application'],data['sw_device']
+    api.init(username,password,country_code,application)
+    device_ids = api.get_all_devices()
+    my_sw_obj = dict((i.name(),i) for i in device_ids if i.obj_type == 'switch' and i.name() == sw_device)
+    result = subprocess.run("ping -c 5 -n 77.88.8.1", shell=True, stdout=subprocess.PIPE, encoding='UTF-8')
+    dev_state = my_sw_obj[sw_device].state()
+    print(dev_state, result.stdout)
+
+    if result.returncode !=0 and dev_state == True:
+        my_sw_obj[sw_device].turn_off()
+        print('Beeline is down!')
+        time.sleep(5)
+        my_sw_obj[sw_device].turn_on()
+        time.sleep(7)
+        dev_state = my_sw_obj[sw_device].state()
+        print(dev_state, 'Router restarted!')
+    elif dev_state == False:
+        my_sw_obj[sw_device].turn_on()
+        print('Switch was disabled! Enabled!')
+    else:
+        print('All is ok!')
+
+
+if __name__ == "__main__":
+    reload_smartplug('/home/phil/tuya/config.json')
